97da3b2efefb6790c22210b0192305d64a22a0d4
Kyle John <njk230@gmail.com>
2017-12-27 00:28:46 +0900

    Upgrade with iOS 11

--- WWCD/WWCD/App.xaml.cs
+++ WWCD/WWCD/App.xaml.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
 
 namespace WWCD
 {
@@ -13,7 +15,14 @@ namespace WWCD
 		{
 			InitializeComponent();
 
-            MainPage = new NavigationPage(new WWCD.MainPage());
+            var page = new Xamarin.Forms.NavigationPage(new MainPage())
+            {
+                BarTextColor = Color.White,
+                BarBackgroundColor = Color.FromHex("#2196F3")
+            };
+            page.On<iOS>().SetPrefersLargeTitles(true);
+
+            MainPage = page;
 		}
 	}
 }
